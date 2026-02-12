4dd40251e4deec61ca41a827d98436225325f6f6
Pierre Fenoll <pierrefenoll@gmail.com>
2026-02-13 00:24:52 +0100

    command queue and homing
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{collections::VecDeque, fmt, fmt::Display, io};
 
 use serial2_tokio::SerialPort;
 use tokio::{
@@ -30,6 +30,9 @@ async fn main() -> Result<(), anyhow::Error> {
     // //action:prompt_end
     // echo:SD card ok
 
+    let mut state =
+        State { reqs: [Req::Heartbeat, Req::IHandleDialogs].into(), ..State::default() };
+
     let mut pos = 0;
     let mut raw = [0u8; 512];
     let mut sig = signal(SignalKind::interrupt())?;
@@ -41,7 +44,7 @@ async fn main() -> Result<(), anyhow::Error> {
                 match r {
                     Ok(0) if pos == 0 => break,
                     Ok(0) => {
-                        handle(&raw[..pos]);
+                        handle(&port, &mut state, &raw[..pos]).await?;
                         break;
                     }
                     Ok(n) => {
@@ -50,7 +53,7 @@ async fn main() -> Result<(), anyhow::Error> {
 
                         let mut start = 0;
                         while let Some(i) = raw[start..pos].iter().position(|&c| c == b'\n') {
-                            handle(&raw[start..(start + i)]);
+                            handle(&port, &mut state, &raw[start..(start + i)]).await?;
                             start += i + 1;
                         }
                         assert!(start <= pos);
@@ -74,10 +77,79 @@ async fn main() -> Result<(), anyhow::Error> {
     Ok(())
 }
 
-fn handle(line: &[u8]) {
-    if let Ok(line) = str::from_utf8(line) {
-        println!("> {line:?}");
-        return;
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum Req {
+    Heartbeat,
+    IHandleDialogs,
+    PenUp(u8 /* 0..180 */, u16),
+    PenDown(u8 /* 0..180 */, u16),
+    MotorsEngage,
+    MotorsDisengage,
+    FindHome,
+}
+impl Display for Req {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Heartbeat => write!(f, "M400"),
+            Self::IHandleDialogs => write!(f, "M876 P1"),
+            Self::PenUp(angle, ms) | Self::PenDown(angle, ms) => {
+                write!(f, "M280 P0 S{angle} T{ms}")
+            }
+            Self::MotorsEngage => write!(f, "M17"),
+            Self::MotorsDisengage => write!(f, "M18"),
+            Self::FindHome => write!(f, "G28 X Y"),
+        }
     }
-    println!("> GARBAGE");
+}
+
+#[derive(Debug, Default)]
+struct State {
+    ready: Option<bool>,
+    reqs: VecDeque<Req>,
+    last_line_number_sent: u32,
+}
+impl State {
+    async fn send(&mut self, port: &SerialPort) -> io::Result<Option<Req>> {
+        if self.ready.is_some_and(|ready| ready)
+            && let Some(req) = self.reqs.pop_front()
+        {
+            self.ready = Some(false);
+            self.last_line_number_sent += 1;
+            println!(">> #{} {req:?}: {req}", self.last_line_number_sent);
+            port.write_all(format!("{req}\n").as_bytes()).await?;
+            return Ok(Some(req));
+        }
+        Ok(None)
+    }
+}
+
+async fn handle(port: &SerialPort, state: &mut State, line: &[u8]) -> anyhow::Result<()> {
+    let Ok(line) = str::from_utf8(line) else {
+        println!("> GARBAGE! Check baud rate? {line:?}");
+        return Ok(());
+    };
+    println!("> {line:?}");
+
+    match line {
+        "//action:notification Polargraph Ready." => {
+            println!("Ready!");
+            assert!(state.ready.is_none(), "State is already initialized: {state:?}");
+            state.ready = Some(true);
+        }
+        "ok" => {
+            println!("   #{} ack'd", state.last_line_number_sent);
+            state.ready = Some(true);
+        }
+        _ => {}
+    }
+
+    if let Some(req) = state.send(port).await? {
+        const PEN_UP: Req = Req::PenUp(90, 250);
+        const PEN_DOWN: Req = Req::PenDown(25, 150);
+        if req == Req::IHandleDialogs {
+            state.reqs.extend([PEN_UP, Req::MotorsDisengage, Req::FindHome].into_iter());
+        }
+    }
+
+    Ok(())
 }
